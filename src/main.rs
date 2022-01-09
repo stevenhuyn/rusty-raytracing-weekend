870d29bcbe8e829f45711f69cae597952b1902eb
stevenhuyn <stevenhuyn@gmail.com>
2022-01-09 11:32:33 -0800

    2.3. Adding a Progress Indicator

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-fn main() {
+use std::{
+    error::Error,
+    io::{stdout, Write},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
     const IMAGE_WIDTH: i32 = 256;
     const IMAGE_HEIGHT: i32 = 256;
 
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScnalines remaining: {}", j);
+        stdout().flush()?;
         for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
@@ -17,4 +24,6 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+
+    Ok(())
 }
