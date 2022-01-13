d816ffbb022b07fd127e69710458de6fad0e79f7
stevenhuyn <stevenhuyn@gmail.com>
2022-01-12 23:16:01 -0800

    Hotfix on 6.5.

--- src/hittable/hittable_list.rs
+++ src/hittable/hittable_list.rs
@@ -8,12 +8,13 @@ impl Hittable for HittableList {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, hit_record: &mut HitRecord) -> bool {
         let mut temp_record: HitRecord = HitRecord::new(); // TODO: Option?
         let mut hit_anything = false;
-        // let mut closest_so_far = t_max;
+        let mut closest_so_far = t_max;
 
         for object in self {
+            // Need to add logic to check which was closest
             if object.hit(ray, t_min, t_max, &mut temp_record) {
                 hit_anything = true;
-                // closest_so_far = temp_record.t;
+                closest_so_far = temp_record.t;
                 *hit_record = temp_record;
             }
         }
