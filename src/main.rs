4d6fb3cca119c3ba7e8bd7bc03d3f12305526d6f
stevenhuyn <stevenhuyn@gmail.com>
2022-01-24 00:36:10 -0800

    Begin using clap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::io::Cursor;
-
-use renderer::draw;
-use window::{render_window, HEIGHT, WIDTH};
-
+use clap::Parser;
 use image::{io::Reader, save_buffer, ColorType, ImageBuffer, RgbImage, RgbaImage};
+use renderer::draw;
+use std::io::Cursor;
+use window::{render_window, HEIGHT, WIDTH};
 
 mod camera;
 mod hittable;
@@ -14,8 +13,26 @@ mod utils;
 mod vec3;
 mod window;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// Name of the person to greet
+    #[clap(short, long)]
+    width: Option<u32>,
+
+    /// Number of times to greet
+    #[clap(short, long)]
+    height: Option<u32>,
+
+    #[clap(short, long)]
+    save: bool,
+
+    #[clap(short, long)]
+    filename: Option<String>,
+}
+
 fn main() {
-    // render_window();
+    // let cli = Cli::parse();
 
     let buffer = draw(WIDTH, HEIGHT);
     save_buffer("image.png", &buffer, WIDTH, HEIGHT, ColorType::Rgba8).unwrap();
