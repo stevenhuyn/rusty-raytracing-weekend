7b10c8097581be7427d7b18647b07b8a451772f5
stevenhuyn <stevenhuyn@gmail.com>
2022-01-25 20:54:39 -0800

    Simplify cli

--- src/main.rs
+++ src/main.rs
@@ -41,20 +41,15 @@ pub const SAMPLE_PER_PIXELS: u32 = 500;
 fn main() {
     let cli = Cli::parse();
     let width = cli.width.unwrap_or(DEFAULT_WIDTH);
-    let height = cli
-        .height
-        .unwrap_or((DEFAULT_WIDTH as f64 / ASPECT_RATIO) as u32);
-    let filename = cli
-        .filename
-        .unwrap_or(format!("image{}x{}.png", width, height));
-    let path = format!("./renders/{}", filename);
+    let height = cli.height.unwrap_or((width as f64 / ASPECT_RATIO) as u32);
+    let filename = cli.filename.unwrap_or("render.png".to_owned());
 
     let now = Instant::now();
     let buffer: Vec<u8> = draw(width, height);
     println!("Rendered in {}", now.elapsed().as_secs_f64());
 
     if cli.save {
-        save_buffer(path, &buffer, width, height, ColorType::Rgba8).unwrap();
+        save_buffer(filename, &buffer, width, height, ColorType::Rgba8).unwrap();
     }
 
     // How to not use handle here?
