3dd838e82c51b61a63862a7fc30c03aa9924172f
stevenhuyn <stevenhuyn@gmail.com>
2022-02-07 17:32:28 -0800

    5.3. Improvement with Hermitian Smoothing

--- src/perlin.rs
+++ src/perlin.rs
@@ -30,9 +30,12 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: Point3) -> f64 {
-        let u = p.x - p.x.floor();
-        let v = p.y - p.y.floor();
-        let w = p.z - p.z.floor();
+        let mut u = p.x - p.x.floor();
+        let mut v = p.y - p.y.floor();
+        let mut w = p.z - p.z.floor();
+        u = u * u * (3.0 - 2.0 * u);
+        v = v * v * (3.0 - 2.0 * v);
+        w = w * w * (3.0 - 2.0 * w);
 
         let i = p.x.floor() as i64;
         let j = p.y.floor() as i64;
